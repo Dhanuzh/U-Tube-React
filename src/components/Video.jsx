a7250650ba32b984bf88eab3a807e1049f4ccbaa
dhanush <dhanush@dhanushs-MacBook-Air.local>
2023-12-27 22:17:16 +0530

    env removed

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Stack, Box } from "@mui/material";
+import VideoCard from "";
 
 const Video = ({ videos }) => {
-  console.log(videos);
-  return <div>video</div>;
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>{item.id.videoId && <VideoCard video={item} />}</Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Video;
