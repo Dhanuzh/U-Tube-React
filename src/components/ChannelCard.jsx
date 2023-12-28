0096f6470d2a300a2f084a4fe9eeab5786f0c6a2
dhanush <dhanush@dhanushs-MacBook-Air.local>
2023-12-28 16:35:57 +0530

    solve video problem

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utilities/constant";
 
-const ChannelCard = () => {
-  return <h1>hello</h1>;
-};
-
+const ChannelCard = ({ channelDetail }) => (
+  <Box
+    sx={{
+      boxShadow: "none",
+      borderRadius: "20px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: { xs: "356px", md: "320px", s: "200px" },
+    }}
+  >
+    <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          color: "#fff",
+        }}
+      >
+        <CardMedia
+          image={
+            channelDetail?.snippet?.thumbnails?.high.url || demoProfilePicture
+          }
+          alt={channelDetail?.snippet?.title}
+          sx={{
+            borderRadius: "50%",
+            width: "180px",
+            height: "180px",
+            mb: 2,
+            border: "3px solid gray",
+          }}
+        />
+        <Typography variant="h6">
+          {channelDetail?.snippet?.title}
+          <CheckCircle sx={{ fontSize: "16px", color: "gray", ml: "5px" }} />
+        </Typography>
+        {channelDetail.statistics?.subscriberCount && (
+          <Typography>
+            {parseInt(
+              channelDetail?.statistics?.subscriberCount,
+            ).toLocaleString()}
+            Subscribers
+          </Typography>
+        )}
+      </CardContent>
+    </Link>
+  </Box>
+);
 export default ChannelCard;
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -12,8 +12,72 @@ import {
   demoProfilePicture,
 } from "../utilities/constant";
 
-const VideoCard = () => {
-  return <h1>Video</h1>;
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Card
+      sx={{
+        width: {
+          md: "320px",
+          xs: "100%",
+        },
+        borderRadius: "0px",
+        boxShadow: "none",
+      }}
+    >
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 240 }}
+        />
+      </Link>
+      <CardContent
+        sx={{
+          backgroundColor: "#1e1e1e",
+          height: { sd: "200px" },
+        }}
+      >
+        <Link to={videoId ? `/video/${videoId - videoId}` : demoVideoTitle}>
+          <Typography
+            variant="subtitle1"
+            fontWeight="bold"
+            color="white"
+            sx={{
+              backgroundColor: "#393837",
+              borderRadius: "5px",
+            }}
+          >
+            {snippet?.title.slice(0, 80) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography
+            variant="subtitle2"
+            fontWeight="300px"
+            sx={{
+              color: "#B9B9B2",
+              mt: "10px",
+            }}
+          >
+            {snippet?.channelTitle.slice(0, 20) ||
+              demoChannelTitle.slice(0, 60)}
+            <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px" }} />
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default VideoCard;
